9883c0915ca8b699e69528706085f4fb7cfe1e2a
ElisabethMazurkiewicz <em034@hdm-stuttgart.de>
2014-12-19 11:40:46 +0100

    ClickHandler fuer registrationButton + onClick Methode (jedoch ohne Operationen)

--- RoomReservationService/src/roomreservationservice/client/RoomReservationService.java
+++ RoomReservationService/src/roomreservationservice/client/RoomReservationService.java
@@ -1,22 +1,20 @@
 package roomreservationservice.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>. 
  */
 
-// test fÃ¼r eli
-
 public class RoomReservationService implements EntryPoint {
-	
-	private VerticalPanel mainPanel = new VerticalPanel ();
-	private Button registrierungsButton = new Button ("Registrieren");
-	private Button authentifizierungsButton = new Button ("Authentifizieren");
-	
+
 	 /**
 	   * "Da diese Klasse die Implementierung des Interface <code>EntryPoint</code>
 	   * zusichert, benï¿½tigen wir eine Methode
@@ -25,12 +23,66 @@ public class RoomReservationService implements EntryPoint {
 	   */
 	public void onModuleLoad() {
 		
+		//Erste Seite. Hauptseite. User hat wahlt zwischen Registrieren und Authentifizieren
+		final VerticalPanel mainPanel = new VerticalPanel();
 		
-		//Assemble Main Panel
-		mainPanel.add(registrierungsButton);
-		mainPanel.add(authentifizierungsButton);
+		//neue Widgets erzeugen. Jeweils ein Button fuer Registrieren und Authentifizieren.
+		final Button registrationButton = new Button ("Registrieren");
+		final Button authentificationButton = new Button ("Authentifizieren");
+		
+		//Hinzufuegen der Buttons zum VerticalPanel "mainPanel"
+		mainPanel.add(registrationButton);
+		mainPanel.add(authentificationButton);
+		
+		/*
+	     * Unter welchem Namen können wir den Button durch die CSS-Datei des
+	     * Projekts formatieren?
+	     * @author:thies
+	     */
+		//registrationButton.setStylePrimaryName("")
+		//authentificationButton.setStylePrimaryName("")
+		
+		/* Verhalten, wenn auf Button geklickt wird.
+		 * Hierzu registrieren wir einen ClickHandler, dessen
+	     * onClick()-Methode beim Mausklick auf den zugehörigen Button aufgerufen
+	     * wird.
+	     */
+		
+		registrationButton.addClickHandler(new ClickHandler() {
+		      
+			public void onClick(ClickEvent event) {
+				
+				//Loescht vorherige Anzeige auf mainPanel.
+				mainPanel.clear ();
+				//TODO Englische Bezeichnungen fuer Textboxen??
+				
+				//Neues Eingabefeld fuer Vorname, Nachname, Email.
+				TextBox forename = new TextBox();
+				TextBox surename = new TextBox();
+				TextBox email = new TextBox();				
+				
+				//Neues Eingabefeld fuer Passwort.
+				PasswordTextBox password = new PasswordTextBox();
+				
+				//neue Buttons fuer weitere Handlungen
+				Button registration = new Button ("Registrieren");
+				Button cancel = new Button ("Abbrechen");
+				
+				//Hinzufuegen der Widgets zum mainPanel
+				mainPanel.add(forename);
+				mainPanel.add(surename);
+				mainPanel.add(email);
+				mainPanel.add(password);
+				mainPanel.add(registration);
+				mainPanel.add(cancel);
+				
+				
+								
+			}
+		});
 		
 		// Main panel mit der HTML Hostpage "RoomReservationService.html" verlinken.
 		RootPanel.get("roomReservationService").add(mainPanel);
-	}
+	
+}
 }
