465bab9d9d9233b8a5b96205a5c8273967ef87e1
ElisabethMazurkiewicz <em034@hdm-stuttgart.de>
2014-12-19 16:10:35 +0100

    Kommentare und TODOs hinzugefuegt.

--- RoomReservationService/src/roomreservationservice/client/RoomReservationService.java
+++ RoomReservationService/src/roomreservationservice/client/RoomReservationService.java
@@ -4,6 +4,7 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
@@ -13,8 +14,9 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  * Entry point classes define <code>onModuleLoad()</code>. 
  */
 
-public class RoomReservationService implements EntryPoint {
 
+public class RoomReservationService implements EntryPoint {
+	private VerticalPanel mainPanel = new VerticalPanel();
 	 /**
 	   * "Da diese Klasse die Implementierung des Interface <code>EntryPoint</code>
 	   * zusichert, benï¿½tigen wir eine Methode
@@ -22,9 +24,13 @@ public class RoomReservationService implements EntryPoint {
 	   * <code>main()</code>-Methode normaler Java-Applikationen." Bankprojekt
 	   */
 	public void onModuleLoad() {
+		// Main panel mit der HTML Hostpage "RoomReservationService.html" verlinken.
+		RootPanel.get("roomReservationService").add(mainPanel);
 		
-		//Erste Seite. Hauptseite. User hat wahlt zwischen Registrieren und Authentifizieren
-		final VerticalPanel mainPanel = new VerticalPanel();
+		/*Erste Seite. Hauptseite. User hat wahlt zwischen Registrieren und 
+		 * Authentifizieren.
+		 * @author:Mazurkiewicz
+		 */
 		
 		//neue Widgets erzeugen. Jeweils ein Button fuer Registrieren und Authentifizieren.
 		Button registrationButton = new Button ("Registrieren");
@@ -42,6 +48,7 @@ public class RoomReservationService implements EntryPoint {
 		//registrationButton.setStylePrimaryName("")
 		//authentificationButton.setStylePrimaryName("")
 		
+		
 		/* Verhalten, wenn auf Button geklickt wird.
 		 * Hierzu registrieren wir einen ClickHandler, dessen
 	     * onClick()-Methode beim Mausklick auf den zugehörigen Button aufgerufen
@@ -54,7 +61,6 @@ public class RoomReservationService implements EntryPoint {
 				
 				//Loescht vorherige Anzeige auf mainPanel.
 				mainPanel.clear ();
-				//TODO Englische Bezeichnungen fuer Textboxen??
 				
 				//Neues Eingabefeld fuer Vorname, Nachname, Email.
 				final TextBox forename = new TextBox();
@@ -80,7 +86,7 @@ public class RoomReservationService implements EntryPoint {
 				      
 					public void onClick(ClickEvent event) {
 						mainPanel.clear ();
-						//TODO als Klasse auslagern kann fuer Login auch verwendet werden!
+						//TODO als Klasse auslagern kann fuer Authentifizierung auch verwendet werden!
 						/*TODO unterscheiden zwischen Registration und Anmeldung, 
 						 * da bei der Anmeldung Email und Passwort ueberprueft werden muessen!
 						 */
@@ -88,17 +94,65 @@ public class RoomReservationService implements EntryPoint {
 						//Neue Buttons fuer die Startseite nach der Registrierung oder Login
 						final Button roomAdministration= new Button ("Raumverwaltung");
 						final Button userAdministration= new Button ("Nutzerverwaltung");
-						final Button reportBuild= new Button ("Raumverwaltung");		
+						final Button reportBuild= new Button ("Raumverwaltung");
 						
-						//Hinzufuegen der Widgets zum mainPanel
-						mainPanel.add(roomAdministration);
-						mainPanel.add(userAdministration);
-						mainPanel.add(reportBuild);
+						/*Erstellen eines HorizontalPanels um Buttons nebeneinander
+						 * anordnen zu koennen.
+						 */
+						HorizontalPanel hPanel = new HorizontalPanel ();
+						
+						//Einfuegen des hPanels in das mainPanel.
+						mainPanel.add(hPanel);
+						
+						//Hinzufuegen der Widgets zum hPanel.
+						hPanel.add(roomAdministration);
+						hPanel.add(userAdministration);
+						hPanel.add(reportBuild);
+						
+						/*TODO ClickHandler fuer roomAdministration, userAdministration
+						 * reportBuild muessen festgelegt werden.								
+						 */
 								
 			}
 		});
 		
-		// Main panel mit der HTML Hostpage "RoomReservationService.html" verlinken.
-		RootPanel.get("roomReservationService").add(mainPanel);
+				cancelButton.addClickHandler(new ClickHandler(){
+					public void onClick(ClickEvent event) {
+						//TODO Leitung zur ersten Seite. Hauptseite. Zeile 30.
+					
+					}});
+				
 };
-		});}}
+
+		});
+		
+		authentificationButton.addClickHandler(new ClickHandler() {
+		      
+			public void onClick(ClickEvent event) {
+				mainPanel.clear ();
+				//Neues Eingabefeld fuer Email.
+				final TextBox email = new TextBox();
+						
+				//Neues Eingabefeld fuer Passwort.
+				PasswordTextBox password = new PasswordTextBox();
+				
+				//neue Buttons fuer weitere Handlungen
+				final Button subsriptionButton = new Button ("Anmelden");
+				final Button cancelButton = new Button ("Abbrechen");
+				
+				//Hinzufuegen der Widgets zum mainPanel
+				mainPanel.add(email);
+				mainPanel.add(password);
+				mainPanel.add(subsriptionButton);
+				mainPanel.add(cancelButton);
+				/*TODO Verwenden des Clickhandlers von registrationButton2 (z.85), 
+				 * aber noch keine Ahnung wie.
+				 */
+				/*TODO Verwenden des Clickhandlers von cancelButton (z.115), 
+				 * aber noch keine Ahnung wie.
+				 */
+				
+				
+			}
+		});
+		}}
