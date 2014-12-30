305ccb7f13d8e7ddc780be7772462c442dc9b7a2
ElisabethMazurkiewicz <em034@hdm-stuttgart.de>
2014-12-30 20:11:29 +0100

    Einfuegen der TODOs.

--- RoomReservationService/src/roomreservationservice/client/gui/InvitationManagement.java
+++ RoomReservationService/src/roomreservationservice/client/gui/InvitationManagement.java
@@ -3,9 +3,11 @@ package roomreservationservice.client.gui;
 /*
  *  GUI-Klasse für die folgenden "Seiten": Einladung die per Mail an User versendet wird.
  *  (Siehe GUI-Prototyp 2.0)
- *  @author: Gertz, Steven
+ **  @author: Steven Gertz, Nicholas Gertz, Elisabeth Mazurkiewicz
  */
 
 public class InvitationManagement {
+	//TODO ClickHandler fuer invitationManagement anlegen 
+	//TODO Buttons und ClickHanlder in Einladungsverwaltung anlegen: Zusagen, Absagen
 
 }
