7377f9a18bf99eb701deb63a8fffc86a74c3e6db
ElisabethMazurkiewicz <em034@hdm-stuttgart.de>
2014-12-30 20:22:26 +0100

    TODOs fuer EventManagement angelegt.

--- RoomReservationService/src/roomreservationservice/client/gui/EventManagement.java
+++ RoomReservationService/src/roomreservationservice/client/gui/EventManagement.java
@@ -7,5 +7,28 @@ package roomreservationservice.client.gui;
  */
 
 public class EventManagement {
-
+	/*TODO Belegung erstellen : 
+	 * Ueberschrift festlegen
+	 * DatePicker: Datum, 
+	 * 1. ListBox: Ort, 
+	 * 2. ListBox: Zeit, 
+	 * 1. CellList: Teilnehmeruebersicht, 
+	 * 2. CellList: Nutzer einladen, 
+	*1. Button+ClickHandler: Belegung uebernehmen
+	*/
+	/*TODO Belegungsuebersicht
+	 * Ueberschrift festlegen
+	 * Kalender darstellen durch Tabelle mit Spalten= Wochentag + Zeilen = Uhrzeit
+	 * 1.Button+ClickHandler: Belegung erstellen
+	 */
+	/*TODO Belegung modifizieren und loeschen
+	 * Ueberschrift festlegen
+	 * DatePicker: Datum, 
+	 * 1. ListBox: Ort, 
+	 * 2. ListBox: Zeit, 
+	 * 1. CellList: Teilnehmeruebersicht, 
+	 * 2. CellList: Nutzer einladen, 
+	 *1. Button+ClickHandler: Aenderung uebernehmen
+	 *2. Button+ClickHandler: Belegung loeschen
+	 */
 }
