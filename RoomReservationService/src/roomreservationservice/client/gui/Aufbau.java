f2a712adfb0f3b9724a7c69b6d879c791c9560c7
ElisabethMazurkiewicz <em034@hdm-stuttgart.de>
2014-12-30 22:44:25 +0100

    Aufbau als Superklasse der Klassen der Navigationsleiste. Um diese Klassen in RoomReservationService.java mittels ClickHandler ueber eine neue Instanz der Klasse die der Superklasse zugeordnet wird un aufgerufen wird, zuzuordnen.

---
+++ RoomReservationService/src/roomreservationservice/client/gui/Aufbau.java
@@ -0,0 +1,69 @@
+package roomreservationservice.client.gui;
+
+import com.google.gwt.user.client.ui.*;
+
+public abstract class Aufbau extends VerticalPanel {
+
+	  /**
+	   * Jedes GWT Widget muss diese Methode implementieren. Sie gibt an, was
+	   * geschehen soll, wenn eine Widget-Instanz zur Anzeige gebracht wird.
+	   */
+	  public void onLoad() {
+	    /*
+	     * Bevor wir unsere eigene Formatierung veranslassen, ueberlassen wir es der
+	     * Superklasse eine Initialisierung vorzunehmen.
+	     */
+	    super.onLoad();
+
+	    /*
+	     * Als erstes geben wir stets die Headline des Showcase aus. Da
+	     * getHeadlineText() als abstrakte Methode bzw. als Einschubmethode
+	     * realisiert wird, obliegt es den Subklassen, für eine Ausgestaltung also
+	     * Implementierung zu sorgen.
+	     */
+	    this.add(this.createHeadline(this.getHeadlineText()));
+
+	    /*
+	     * Wenn alles vorbereitet ist, stoßen wir die run()-Methode an. Auch run()
+	     * ist als abstrakte Methode bzw. als Einschubmethode realisiert. Auch hier
+	     * ist es Aufgabe der Subklassen, für deren Implementierung zu sorgen.
+	     */
+	    this.run();
+	  }
+
+	  /**
+	   * Mit Hilfe dieser Methode erstellen wir aus einem String ein mittels CSS
+	   * formatierbares HTML-Element. 
+	   * 
+	   * @param text der String, den wir als andernorts HTML setzen wollen.
+	   * @return GWT HTML Widget.
+	   */
+	  protected HTML createHeadline(String text) {
+	    HTML content = new HTML(text);
+	    return content;
+	  }
+
+	  /**
+	   * Mit Hilfe dieser Methode erstellen wir aus einem Strinng ein mittels CSS
+	   * formatierbares HTML-Element, das an das Ende der bisherigen Ausgabe dieser
+	   * Reaktion angehaengt wird. 
+	   * 
+	   * @param text der String, den wir als HTML an die bisherige Showcase-Ausgabe
+	   *          anhängen wollen.
+	   */
+	  protected void append(String text) {
+	    HTML content = new HTML(text);
+	    this.add(content);
+	  }
+
+	  /**
+	   * Abstrakte Einschubmethode, die in den Subklassen zu realisieren ist.
+	   * @return der Text, den wir als Headline setzen wollen. 
+	   */
+	  protected abstract String getHeadlineText();
+
+	  /**
+	   * Abstrakte Einschubmethode, die in den Subklassen zu realisieren ist.
+	   */
+	  protected abstract void run();
+	}
