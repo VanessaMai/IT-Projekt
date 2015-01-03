e712b5dee93c99fe69dc7b139735293527118e02
ElisabethMazurkiewicz <em034@hdm-stuttgart.de>
2015-01-03 12:41:23 +0100

    TODOs bei Klassen Logout und Hompage.

---
+++ RoomReservationService/src/roomreservationservice/client/gui/Hompage.java
@@ -0,0 +1,8 @@
+package roomreservationservice.client.gui;
+
+public class Hompage {
+	/*TODO Aufrufen der Startseite
+	 * wird auch in General nach anmeldung aufgerufen werden.
+	 */
+
+}
---
+++ RoomReservationService/src/roomreservationservice/client/gui/Logout.java
@@ -0,0 +1,8 @@
+package roomreservationservice.client.gui;
+
+public class Logout {
+	/*TODO Abmeldung. Was passiert dabei?
+	 * 
+	 */
+
+}
