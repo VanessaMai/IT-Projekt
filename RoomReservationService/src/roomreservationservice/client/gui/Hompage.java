94f294463acd83b7fb35cf4ff690f545ac5fecd5
NicholasGertz <ng034@hdm-stuttgart.de>
2015-01-04 17:28:27 +0100

    Ausarbeitung der Startseite - Text Label und Grid hinzugefügt

--- RoomReservationService/src/roomreservationservice/client/gui/Hompage.java
+++ RoomReservationService/src/roomreservationservice/client/gui/Hompage.java
@@ -1,15 +1,41 @@
 package roomreservationservice.client.gui;
 
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Label;
+
+/*
+ *  GUI-Klasse fuer die Startseite.
+ *  (Siehe GUI-Prototyp 2.0)
+ *  @author: Nicholas Gertz
+ */
+
 public class Hompage extends Reaction{
 
 	@Override
+
 	protected String getHeadlineText() {
 		return "Startseite";
 	}
 
+	/**
+	   * Jede Reaction muss die <code>run()</code>-Methode implementieren. Sie ist
+	   * eine "Einschubmethode", die von einer Methode der Basisklasse
+	   * <code>Reaction</code> aufgerufen wird, wenn die Reaction aktiviert wird.
+	   */
+	
 	@Override
 	protected void run() {
-		// TODO Auto-generated method stub
+		
+	/*Anlegen einer 2x1 Matrix (Grid). Diese wird mit einem String als Textausgabe gefuehlt.
+	 */	
+		
+		Grid HomepageGrid = new Grid(2, 1);
+		this.add(HomepageGrid);
+		
+		Label HomepageLabel = new Label("Willkommen auf der Startseite. Bitte nutzen Sie die Navigationsleiste, um.... ");
+	
+		HomepageGrid.add(HomepageLabel);
+		
 		
 	}
 	/*TODO Aufrufen der Startseite
