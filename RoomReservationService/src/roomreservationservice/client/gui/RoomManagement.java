2ffac6ba7ae652615f804f27c70b510ee36266cf
NicholasGertz <ng034@hdm-stuttgart.de>
2015-01-04 23:08:14 +0100

    Klassen auf Prototyp 2.0 akutalisiert. Grids, Widgets und ClickHandler integriert. TODOS akutalisiert

--- RoomReservationService/src/roomreservationservice/client/gui/RoomManagement.java
+++ RoomReservationService/src/roomreservationservice/client/gui/RoomManagement.java
@@ -1,5 +1,12 @@
 package roomreservationservice.client.gui;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
 /*
  *  GUI-Klasse für die folgenden "Seiten": Raum wählen, Raum modifizieren/löschen,
  *  Raum erstellen. (Siehe GUI-Prototyp 2.0)
@@ -13,27 +20,64 @@ public class RoomManagement extends Reaction{
 		return "Raumverwaltung";
 	}
 
+
 	@Override
 	protected void run() {
 		// TODO Auto-generated method stub
 		
-	}
-	/*TODO RaumVerwaltun Hauptseite:
-	 * CellList: Raum waehlen
-	 * 1. Button + ClickHandler: Raum modifizieren
-	 * 2. Button + ClickHandler: Raum erstellen
+		/*TODO RaumVerwaltun Hauptseite:
+		 * CellList: Raum waehlen
+		 * 1. Button + ClickHandler: Raum modifizieren
+		 * 2. Button + ClickHandler: Raum erstellen
+		
+	/*Anlegen einer 3x2 Matrix (Grid). Diese wird Buttons gefühlt.
+	 */
+	Grid RoomManagementGrid = new Grid(3, 2);
+	this.add(RoomManagementGrid);
+	
+	/* Buttons um auf die Seiten "Raum modifizieren" und "Raum erstellen" zu gelangen
+	 */
+	Button modify = new Button ("Raum modifizieren");
+	Button setup = new Button ("Raum erstellen");
+	
+	/*Buttons im Grid platzieren
+	 */
+	RoomManagementGrid.setWidget(0, 0, modify);
+	RoomManagementGrid.setWidget(0, 1, setup);
+
+	/*Clickhandler für die Buttons
 	 */
 	
+	  modify.addClickHandler(new ClickHandler() {
+		    public void onClick(ClickEvent event) {
+		      // handle the click event
+		    }
+		  });
+	
+	setup.addClickHandler(new ClickHandler() {
+		public void onClick(ClickEvent event) {
+			// handle the click event
+			}
+		});
+	}
+}
+
+	
+	
+	
+
 	/*TODO Raum modifizieren und loeschen:
 	 * 1. TextBox: Namen aendern
 	 * 2. TextBox: Kapazitaet aendern
 	 * 1. Button + ClickHandler: Raum modifizieren
 	 * 2. Button + ClickHandler: Raum loeschen
 	 */
+	
+	
+	
 	/*TODO Raum erstellen:
 	 * 1. TextBox: Namen eingeben
 	 * 2. TextBox: Kapazitaet aendern
 	 * 1. Button + ClickHandler: Raum erstellen
 	 */
 
-}
